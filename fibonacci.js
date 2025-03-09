247dca07735b66352ea8623f4f9ddf8d00aa6d41
dachieu2k2 <hieutedam2k2@gmail.com>
2025-03-09 15:27:55 +0700

    :scream: feat: add fibo

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+function fibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0,
+    b = 1,
+    temp;
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
+
+console.time("Execution Time");
+let result = fibonacci(100);
+console.timeEnd("Execution Time");
+console.log("Fibonacci(100):", result);
